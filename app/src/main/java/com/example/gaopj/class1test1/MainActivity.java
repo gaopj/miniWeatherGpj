d40cd3b238f115cd16233aeb38c465b7c94c3a5e
gaopiji <gaopj@pku.edu.cn>
2016-09-21 08:01:32 +0800

    init project

---
+++ app/src/main/java/com/example/gaopj/class1test1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gaopj.class1test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lenovo on 2016/9/20.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
